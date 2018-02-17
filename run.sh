10e1af91665fabca00e9d7cb488aa88f40932e86
Anson Foong <afoong@albany.edu>
2018-02-17 05:23:25 -0500

    Rewriting the script with more proper structure

---
+++ run.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+lineCounter=1
+unzip_FILE()
+{
+  unzip $FILE
+  echo "Extraction Successful... [$(date)]" 1>>log_output.txt
+  return
+}
+compile_FILE()
+{
+  javac $(find */ -name "*.java")
+  echo "Compile Successful... [$(date)]" 1>> log_output.txt
+  return
+}
+
+cd_src()
+{
+  cd */src
+  echo "Here"
+  return
+}
+run_FILE()
+{
+
+  #RUNNABLE_FILE_COUNT=$(grep -l 'void main' $(find */ -name "*.java") | wc -l) # This will return the number of files to be executed.
+  counter=0
+  # Loop through and get the next file to be executed.
+  while [[ "$counter" = 0 ]];
+  do
+
+    #echo "Enter the name of the package"
+    #read PACKAGE
+    #java "$PACKAGE.$(grep -l 'void main' $(find */ -name "*.java") | xargs -L 1 basename | sed -n "$counter #p" | cut -f 1 -d '.')"
+    EXECUTE_FILE # This will always guarantee it will run once.
+    echo "Enter 1 to stop"
+    read CHOICE
+    if [[ "$CHOICE" != "1" ]];
+    then
+      EXECUTE_FILE # If they do not enter 1, then we continue to grade.
+
+    else
+      counter=$(($counter+1))
+    fi
+  done
+
+}
+
+EXECUTE_FILE()
+{
+
+  echo "Enter the full name of the package"
+  read PACKAGE
+  java "$PACKAGE.$(grep -l 'void main' $(find */ -name "*.java") | xargs -L 1 basename | sed -n "$lineCounter p" | cut -f 1 -d '.')" # Run the file.
+  lineCounter=$(($lineCounter+1))
+  return # Return.
+}
+
+find_MAIN()
+{
+  grep -l 'void main' $(find */ -name "*.java") -l | xargs -L 1 basename | sed -n '1 p'
+}
+echo "Welcome to Anson's Java Compiling Script. Please enter an existing file to extract"
+read FILE
+if [ -e "$FILE" ]; # If the file exists, we want to unzip it.
+then
+  unzip_FILE # Unzip the file.
+  compile_FILE # Compile the java files in the project folder.
+  #After compiling, go into the src folder.
+  find_MAIN
+  cd_src # At this point everything has been compiled. We just need to run the file.
+  run_FILE
+
+fi
+cd -
+rm -r */
+#java $(grep -l 'void main' $(find . -name "*.java") | cut -f 2 -d '.')
