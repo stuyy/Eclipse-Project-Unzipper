99cd25c505086226f7831b6949a1f11169f2ccac
Anson Foong <afoong@albany.edu>
2018-02-16 22:52:19 -0500

    Looping through each .java file

--- script.sh
+++ script.sh
@@ -13,7 +13,15 @@ then
   mv $(find . -name "*.java") ./ # Move all the java files to the cwd.
   sed -ie '\|com.ualbany|d' *.java
   javac *.java
-  java $(grep -l main *.java | cut -f 1 -d '.' | head -1)
+  fileCount=$(grep -l 'void main' *.java | cut -f 1 -d '.' | wc -l)
+  echo $fileCount
+  counter=1
+  while [[ "$counter" -le "$fileCount" ]];
+  do
+    java $(grep -l 'void main' *.java | cut -f 1 -d '.' | sed -n "$counter p")
+    counter=$(($counter+1))
+  done
+  #java $(grep -l main *.java | cut -f 1 -d '.' | head -1)
   echo "End of test, Deleting files..."
   rm *.java *.class *.javae # Delete all the .java and .class files
   rm -r */ # Delete all directories in the current working directory.
