8a2dfe89a79d2c48ba11f1fea211a044eb3f0ff7
Anson Foong <afoong@albany.edu>
2018-02-18 18:27:09 -0500

    Rewrote the RUN_FILE method, the user must enter the name of the file with the main method

--- run.sh
+++ run.sh
@@ -24,43 +24,19 @@ cd_src()
 }
 run_FILE()
 {
-
-  #RUNNABLE_FILE_COUNT=$(grep -l 'void main' $(find */ -name "*.java") | wc -l) # This will return the number of files to be executed.
-  counter=0
-  # Loop through and get the next file to be executed.
-  while [[ "$counter" = 0 ]];
-  do
-
-    #echo "Enter the name of the package"
-    #read PACKAGE
-    #java "$PACKAGE.$(grep -l 'void main' $(find */ -name "*.java") | xargs -L 1 basename | sed -n "$counter #p" | cut -f 1 -d '.')"
-    EXECUTE_FILE # This will always guarantee it will run once.
-    echo "Enter 1 to stop"
-    read CHOICE
-    if [[ "$CHOICE" != "1" ]];
-    then
-      EXECUTE_FILE # If they do not enter 1, then we continue to grade.
-
-    else
-      counter=$(($counter+1))
-    fi
-  done
-
-}
-
-EXECUTE_FILE()
-{
-  pwd
-  echo "Enter the full name of the package"
+  echo "Enter the name of the package"
   read PACKAGE
-  echo $PACKAGE
-  java "$PACKAGE.$(grep -l 'void main' $(find */ -name "*.java") | xargs -L 1 basename | sed -n "$lineCounter p" | cut -f 1 -d '.')" # Run the file.
-  lineCounter=$(($lineCounter+1))
-  return # Return.
+  echo "Enter the name of the file"
+  read FILE
+  java "$PACKAGE.$FILE"
+  echo "Continue? y/n"
+  read CHOICE
+  if [[ "$CHOICE" = "y" || "$CHOICE" = "Y" ]];
+  then
+    run_FILE
+  fi
+  return
 }
-
-
-
 
 echo "Welcome to Anson's Java Compiling Script. Please enter an existing file to extract"
 read FILE
@@ -69,8 +45,11 @@ then
   unzip_FILE # Unzip the file.
   compile_FILE # Compile the java files in the project folder.
   #After compiling, go into the src folder.
+
   cd_src # At this point everything has been compiled. We just need to run the file.
+  # I want the user to enter the name of the file they're compiling.
   run_FILE
+  
 fi
 cd -
 rm -r */
